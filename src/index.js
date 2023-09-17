fac8d9beb0d27f7bbbc6b26a1e3917daacaa2f09
MakhmutT <97080565+MakhmutT@users.noreply.github.com>
2023-09-17 19:28:44 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react'
+
+import './index.css';
+import App from './App';
+
+import { persistor, store } from './store'
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
